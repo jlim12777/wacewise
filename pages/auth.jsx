2d7a45c3710b87f432a6d24d453456f83bd00aad
jlim12777 <Justin.2210@icloud.com>
2025-07-24 01:31:15 +0800

    Update auth.jsx

--- pages/auth.jsx
+++ pages/auth.jsx
@@ -1,1 +1,8 @@
-<PASTE CONTENT FROM AUTH AND STRIPE>
+export default function AuthPage() {
+  return (
+    <main style={{ textAlign: 'center', paddingTop: '100px' }}>
+      <h1>🔐 Auth Page</h1>
+      <p>This is where users will log in or sign up.</p>
+    </main>
+  );
+}

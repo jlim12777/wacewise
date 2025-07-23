c94edebed1acaba6418b4f2285151ba492a56815
jlim12777 <Justin.2210@icloud.com>
2025-07-24 01:31:38 +0800

    Update library.jsx

--- pages/library.jsx
+++ pages/library.jsx
@@ -1,1 +1,8 @@
-<PASTE CONTENT FROM PRACTICE LIBRARY>
+export default function LibraryPage() {
+  return (
+    <main style={{ textAlign: 'center', paddingTop: '100px' }}>
+      <h1>📚 Library Page</h1>
+      <p>View your practice resources here.</p>
+    </main>
+  );
+}
